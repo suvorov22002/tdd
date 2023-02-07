a8e883a2fef627df442611ee56a568567b7d5a39
Rodrigue Toukam <rodrigue_toukam@afrilandfirstbank.com>
2023-02-07 14:56:57 +0100

    initial commit

---
+++ src/test/java/com/usine/tdd/TddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usine.tdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/com/usine/tdd/ToDoControllerTest.java
@@ -0,0 +1,5 @@
+package com.usine.tdd;
+
+public class ToDoControllerTest {
+
+}
